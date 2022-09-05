ce056f7d70f5b760911f815623f0860a6ea8f6c6
SteveCelebrates <44465111+SteveCelebrates@users.noreply.github.com>
2022-09-05 17:44:39 +0300

    Added Webapi.xml, Post interface, and Reg file

---
+++ Smsnow/Api/PostSmsInterface.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * SteveCelebartes Free Software
+ * 
+ * @category CelSms_Smsnow
+ * 
+ * @author SteveCelebrates
+ * 
+ * Copyright © Steve Celebrates (2022), Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+namespace CelSms\Smsfree\Api;
+
+interface PostSmsInterface
+{
+    /**
+     * POST api for Order placed notification
+     * @param string $orderid
+     * @param string $message
+     * @return string
+     */
+
+    public function placedOrder($orderid, $message);
+
+    /**
+     * POST api for Order shipped notification
+     * @param string $orderid
+     * @param string $message
+     * @return string
+     */
+
+    public function shippedOrder($orderid, $message);
+}
---
+++ Smsnow/registration.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * SteveCelebartes Free Software
+ * 
+ * @category CelSms_Smsnow
+ * 
+ * @author SteveCelebrates
+ * 
+ * Copyright © Steve Celebrates (2022), Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'CelSms_Smsnow', __DIR__);
